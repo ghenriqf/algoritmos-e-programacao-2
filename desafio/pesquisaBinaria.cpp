a69b4b59c9addb79f36dabfec9ec1064d925da3a
Gabriel Henrique <160789094+ghenriqf@users.noreply.github.com>
2025-08-29 12:16:17 -0300

    Update pesquisaBinaria.cpp

--- desafio/pesquisaBinaria.cpp
+++ desafio/pesquisaBinaria.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 using namespace std;
 
-bool pesquisaBinaria(int num,int arr[5]) {
-    int tamanho = sizeof(arr); 
-    int maior = arr[tamanho];
-    int menor = arr[0];
-    int meio = (maior + menor) / 2;
+bool pesquisaBinaria(int num, int arr[], int tamanho) {
+    int inicio = 0;
+    int fim = tamanho - 1;
 
-    while(true) {
-        if (num > meio) {
-            menor = meio;
-            meio = (maior + menor) / 2;
-        } else if (num < meio) {
-            maior = meio;
-            meio = (menor + maior) / 2;
-        } else if (num == meio) {
-            return true;    
+    while (inicio <= fim) {
+        int meio = (inicio + fim) / 2;
+
+        if (arr[meio] == num) {
+            return true;
+        } else if (arr[meio] < num) {
+            inicio = meio + 1;
+        } else {
+            fim = meio - 1;
         }
     }
+    return false;
 }
 
-int main () {
-    int arr[5] = {2,5,8,13,25};
+int main() {
+    int arr[5] = {2, 5, 8, 13, 25};
     int num;
-    cout << ":";
+    cout << "Digite um numero: ";
     cin >> num;
-    cout << (pesquisaBinaria(num,arr));
+
+    if (pesquisaBinaria(num, arr, 5)) {
+        cout << "Encontrado!" << endl;
+    } else {
+        cout << "Nao encontrado!" << endl;
+    }
 }
